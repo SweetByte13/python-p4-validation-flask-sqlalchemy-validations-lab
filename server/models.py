8641aaabe15ef2bb44dd5d07f0fb7c5abfcf8256
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-06-09 08:31:22 -0400

    done

--- server/models.py
+++ server/models.py
@@ -11,8 +11,20 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
-
+    @validates('name')
+    def validates_name(self, key, name):
+        if not name:
+            raise ValueError("Author must have a name")
+        elif Author.query.filter(Author.name == name).first():
+            raise ValueError("Authors name must be unique")
+        return name
+    
+    @validates('phone_number')
+    def validates_phone_number(self, key, phone_number):
+        if not phone_number.isdigit() or len(phone_number) != 10:
+            raise ValueError("Author must have a valid phone number with exactly ten digits.")
+        return phone_number
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,8 +39,30 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('content')
+    def validates_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Post must be at least 250 characters long.")
+        return content
+    
+    @validates('summary')
+    def validates_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Summary must be greater than 250 characters long.")
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be either Fiction or Non-Fiction.")
+        return category
 
+    @validates('title')
+    def validates_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError("Post title must contain one of the following: 'Won't Believe', 'Secret', 'Top', 'Guess'.")
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
